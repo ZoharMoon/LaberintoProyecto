46cf9bb476c9ef88fe031c8f5f2c268c4e5d30ea
Zohar Calderon <ilovepets223@gmail.com>
2023-09-11 18:49:40 -0500

    Salir del programa con UP

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 nombre = input("Escribe tu nombre: ")
 print("Bienvenido a mi laberinto",nombre)
+
+import readchar
+
+print("El programa está en ejecución. Presiona la tecla '↑' para salir.")
+
+while True:
+    char = readchar.readkey()
+    if char == readchar.key.UP:
+        print("Tecla '↑' presionada. Saliendo del programa.")
+        break
+    print(f"Tecla '{char}' presionada.")
